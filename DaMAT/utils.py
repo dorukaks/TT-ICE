1f9911858d6ad63c33b3e3b6b4586b337968e3f6
dorukaks <doruk@umich.edu>
2022-11-26 15:42:31 -0500

    fix(utils): remove unnecessary imports

--- DaMAT/utils.py
+++ DaMAT/utils.py
@@ -1,8 +1,9 @@
 """ This file contains the utility functions for running TT-ICE pack"""
 
 import numpy as np
-import dask.array as da
-from datetime import datetime
+
+# import dask.array as da
+# from datetime import datetime
 
 
 def primes(n):
@@ -54,43 +55,43 @@ def deltaSVD(data, dataNorm, dimensions, eps=0.1):
     return u[:, :truncationRank], s[:truncationRank], v[:truncationRank, :]
 
 
-def ttsvdLarge(A, eps=0.1):
-    """Tensor-Train decomposition using SVD for large matrices using dask serialization
-    check Oseledets 2011 for further information on ttsvd algorithm"""
-    input_shape = A.shape
-    dims = len(A.shape)
-    # delta = (eps / ((dims - 1) ** (0.5))) * np.linalg.norm(A)
-    r = [1]
-    cores = []
-    for k in range(dims - 1):
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d_%H%M")
-        print(f"k:{k}  {timestamp}")
-        nk = input_shape[k]
-        A = A.reshape((r[k] * nk, int(np.prod(A.shape) / (r[k] * nk))), order="F")
-        A = da.from_array(A)
-        uda, sda, vhda = da.linalg.svd_compressed(A, k=1e100)
-        sigma = sda.compute()
-        svallist = list(sigma * sigma)
-        svallist.reverse()
-        # truncpost = [
-        #     idx
-        #     for idx, element in enumerate(np.cumsum(svallist))
-        #     if element <= delta**2
-        # ]
-        u = uda.compute()
-        del uda
-        cores.append(u[:, : r[k + 1]].reshape((r[k], nk, r[k + 1]), order="F"))
-        del u
-        vh = vhda.compute()
-        del vhda, sda
-        A = np.zeros_like(vh[: r[k + 1], :])
-        for idx, si in enumerate(sigma[0 : r[k + 1]]):
-            A[idx, :] = si * vh[idx, :]
-        del vh
-    r.append(1)
-    cores.append(A.reshape((r[-2], input_shape[-1], r[-1]), order="F"))
-    return r, cores
+# def ttsvdLarge(A, eps=0.1):
+#     """Tensor-Train decomposition using SVD for large matrices using dask serialization
+#     check Oseledets 2011 for further information on ttsvd algorithm"""
+#     input_shape = A.shape
+#     dims = len(A.shape)
+#     # delta = (eps / ((dims - 1) ** (0.5))) * np.linalg.norm(A)
+#     r = [1]
+#     cores = []
+#     for k in range(dims - 1):
+#         now = datetime.now()
+#         timestamp = now.strftime("%Y%m%d_%H%M")
+#         print(f"k:{k}  {timestamp}")
+#         nk = input_shape[k]
+#         A = A.reshape((r[k] * nk, int(np.prod(A.shape) / (r[k] * nk))), order="F")
+#         A = da.from_array(A)
+#         uda, sda, vhda = da.linalg.svd_compressed(A, k=1e100)
+#         sigma = sda.compute()
+#         svallist = list(sigma * sigma)
+#         svallist.reverse()
+#         # truncpost = [
+#         #     idx
+#         #     for idx, element in enumerate(np.cumsum(svallist))
+#         #     if element <= delta**2
+#         # ]
+#         u = uda.compute()
+#         del uda
+#         cores.append(u[:, : r[k + 1]].reshape((r[k], nk, r[k + 1]), order="F"))
+#         del u
+#         vh = vhda.compute()
+#         del vhda, sda
+#         A = np.zeros_like(vh[: r[k + 1], :])
+#         for idx, si in enumerate(sigma[0 : r[k + 1]]):
+#             A[idx, :] = si * vh[idx, :]
+#         del vh
+#     r.append(1)
+#     cores.append(A.reshape((r[-2], input_shape[-1], r[-1]), order="F"))
+#     return r, cores
 
 
 def ttsvd(data, dataNorm, eps=0.1, dtype=np.float32):
