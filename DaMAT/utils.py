f0821213f3ff8933e7a770fde75a564dd8d394c3
dorukaks <doruk@umich.edu>
2022-12-06 09:26:02 -0500

    docs(utils): write docstrings for TTSVD function

--- DaMAT/utils.py
+++ DaMAT/utils.py
@@ -35,6 +35,39 @@ def coreContraction(cores):
 
 
 def deltaSVD(data, dataNorm, dimensions, eps=0.1):
+    """
+    Performs delta-truncated SVD similar to that of the `TTSVD`_ algorithm.
+
+    Given an unfolding matrix of a tensor, its norm and the number of dimensions
+    of the original tensor, this function computes the truncated SVD of `data`.
+    The truncation error is determined using the dimension dependant _delta_ formula
+    from `TTSVD`_ paper.
+
+    Parameters
+    ----------
+    data:obj:`numpy.array`
+        Matrix for which the truncated SVD will be performed.
+    dataNorm:obj:`float`
+        Norm of the matrix. This parameter is used to determine the truncation bound.
+    dimensions:obj:`int`
+        Number of dimensions of the original tensor. This parameter is used to determine
+        the truncation bound.
+    eps:obj:`float`, optional
+        Relative error upper bound for TT-decomposition.
+
+    Returns
+    -------
+    u:obj:`numpy.ndarray`
+        Column-wise orthonormal matrix of left-singular vectors. _Truncated_
+    s:obj:`numpy.array`
+        Array of singular values. _Truncated_
+    v:obj:`numpy.ndarray`
+        Row-wise orthonormal matrix of right-singular vectors. _Truncated_
+
+    .. _TTSVD:
+        https://epubs.siam.org/doi/epdf/10.1137/090752286
+    """
+
     # TODO: input checking
 
     # perform delta-truncated svd similar to that of the ttsvd algorithm
